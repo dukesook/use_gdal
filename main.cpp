eb6c9564c9682ebe45ac24006381f50579aa1a4c
dukesook <devonsookhoo14@gmail.com>
2024-03-27 02:30:07 -0600

    using gdalgs!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
-
+#include <gdal.h>
+#include <gdal_priv.h>
 #include <iostream>
+
 using namespace std;
+
 int main() {
-    cout << "Hello, World!" << endl;
+  cout << "using gdal";
+    GDALDataset *poDataset;
+
+    GDALAllRegister();
+
+    // // Open a dataset, for example
+    // poDataset = (GDALDataset *) GDALOpen( "your_dataset.tif", GA_ReadOnly );
+    // if( poDataset == NULL ) {
+    //     // Handle error
+    // }
+
+    // // Your GDAL operations here
+
+    // // Clean up
+    // GDALClose(poDataset);
     return 0;
 }
